dd2fd0fa49b4f562386f08ff7e7826a0671ebfb7
Daniel Feng <d.feng@processand.com>
2019-11-22 17:59:49 +0800

    update

--- uploadDropbox.js
+++ uploadDropbox.js
@@ -4,6 +4,7 @@ const dropbox = dropboxV2Api.authenticate({ token: process.env.DROPBOX_TOKEN });
 const mode = (!!process.env.MODE) ? process.env.MODE : 'dev';
 
 (() => {
+    console.log(`upload file to ${mode}/intelligent-data-hub-linux`);
     // overwrite seems not to work for binaries, thus the file will be deleted before upload
     dropbox({
         resource: 'files/delete',
